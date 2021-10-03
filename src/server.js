5eb035c7c9c36eb3eb32ec9ecda661012c44f03a
minung.han <hmu332233@gmail.com>
2021-10-04 02:56:41 +0900

    web socket server 설정

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -7,5 +9,11 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000 ws://localhost:3000`);
+
+// http server
+const server = http.createServer(app);
+// web socket server
+const wss = new WebSocket.Server({ server }); // http server 없이 web socket server만 띄워도 된다.
+
+server.listen(3000, handleListen);
