a25f19ce33f874163e27f605440ff3ed9e96cfd0
minung.han <hmu332233@gmail.com>
2021-10-10 02:05:39 +0900

    webrtc project init

---
+++ src/socketio_src/server.js
@@ -0,0 +1,19 @@
+import http from 'http';
+import SocketIo, { Server } from 'socket.io';
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000 ws://localhost:3000`);
+
+// http server
+const server = http.createServer(app);
+// socket server
+const io = SocketIo(server);
+
+server.listen(3000, handleListen);
