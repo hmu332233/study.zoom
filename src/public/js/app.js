9a72ab78dc5678de8acc0992315d2071d560a276
minung.han <hmu332233@gmail.com>
2021-10-10 03:10:57 +0900

    Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,21 +4,41 @@ const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 
+const camerasSelect = document.getElementById('cameras');
+
 let muted = false;
 let cameraOff = false;
 
-(async () => {
-  // const myStream = await getMedia();
-  // myFace.srcObject = myStream;
-})();
+
+getMedia();
+
+async function getCameras() {
+  try {
+    // 사용가능한 디바이스 목록 가져오기
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter(device => device.kind === 'videoinput');
+
+    cameras.forEach(camera => {
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+
+      camerasSelect.appendChild(option);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function getMedia() {
   try {
-    return await navigator.mediaDevices.getUserMedia({
+    // 미디어 스트림 가져오기
+    myStream = await await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-
+    myFace.srcObject = myStream;
+    await getCameras();
   } catch(err) {
     /* 오류 처리 */
     console.error(err);
@@ -27,6 +47,11 @@ async function getMedia() {
 
 
 function handleMuteClick() {
+  // audio track enabled에 따라 소리 제어가 가능
+  myStream
+    .getAudioTracks()
+    .forEach(track => track.enabled = !track.enabled);
+  
   if (!muted) {
     muteBtn.innerHTML = 'Unmute';
     muted = true;
@@ -37,6 +62,11 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+  // camera track enabled에 따라 소리 제어가 가능
+  myStream
+    .getVideoTracks()
+    .forEach(track => track.enabled = !track.enabled);
+
   if (cameraOff) {
     cameraBtn.innerText = 'Turn Camera Off';
     cameraOff = false;
