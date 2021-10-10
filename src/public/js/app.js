9b05c68eceba01611d9481599405791344711bcf
minung.han <hmu332233@gmail.com>
2021-10-10 18:32:40 +0900

    Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -17,12 +17,14 @@ async function getCameras() {
     // 사용가능한 디바이스 목록 가져오기
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter(device => device.kind === 'videoinput');
-
+    const currentCamera = myStream.getVideoTracks()[0]; // 어떤 카메라가 선택되어있는지 알 수 있음
     cameras.forEach(camera => {
       const option = document.createElement('option');
       option.value = camera.deviceId;
       option.innerText = camera.label;
-
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (err) {
@@ -30,15 +32,26 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: 'user' }, // 모바일의 전면 카메라를 우선 사용
+  }
+
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId }},
+    // exact가 있으면 해당 deviceId로 device가 있으면 화면 표시 없으면 표시하지 않음
+    // exact가 없으면 해당 deviceId로 device가 없어도 다른 device로 화면 표시
+  }
   try {
     // 미디어 스트림 가져오기
-    myStream = await await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+
+    getCameras();
   } catch(err) {
     /* 오류 처리 */
     console.error(err);
@@ -76,5 +89,10 @@ function handleCameraClick() {
   }
 }
 
+function handleCameraChange() {
+  getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
+camerasSelect.addEventListener('change', handleCameraChange);
